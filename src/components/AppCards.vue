49e7c446c4a80dcd7fc0a3fc8656a9c54116a074
wmancaniello <wmancaniello@gmail.com>
2024-05-12 11:35:50 +0200

    Add and fix Flip Card, upload flag img

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -17,27 +17,65 @@ export default {
         ? "https://image.tmdb.org/t/p/w300" + this.cardObj.poster_path
         : "src/assets/img/Icona-Cristo.jpg";
     },
+    getOverview() {
+      return this.cardObj.overview
+    }
   },
 };
 </script>
 
 <template>
-  <div class="container mt-3 bg-secondary">
+  <div class="container mt-3">
     <!-- Front Card -->
-    <div>
-      <img :src="getPoster" :alt="cardTitle" />
-    </div>
-    <!-- /Front Card -->
+    <div class="card">
+      <div class="front">
+        <img :src="getPoster" :alt="cardTitle" />
+      </div>
+      <!-- /Front Card -->
 
-    <!-- Back Card -->
-    <div>
-      <h4>Titolo: {{ cardTitle }}</h4>
-      <p>Titolo Originale {{ cardOriginalName }}</p>
-      <p>Lingua Originale: {{ cardObj.original_language }}</p>
-      <p>Voto: {{ cardObj.vote_average }}</p>
+      <!-- Back Card -->
+      <div class="back">
+        <h4>Titolo: {{ cardTitle }}</h4>
+        <p>Titolo Originale {{ cardOriginalName }}</p>
+        <p>Lingua Originale: {{ cardObj.original_language }}</p>
+        <p>Voto: {{ cardObj.vote_average }}</p>
+        <p>Overview: {{ getOverview }} </p>
+      </div>
+      <!-- /Back Card -->
     </div>
-    <!-- /Back Card -->
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  height: 100%;
+  width: 100%;
+  border: 0;
+  position: relative;
+  transition: transform 1500ms;
+  transform-style: preserve-3d;
+
+  .back {
+    height: 100%;
+    width: 100%;
+    border-radius: 2rem;
+    position: absolute;
+    text-align: center;
+    background: linear-gradient(135deg, #1a1a1a, #333333);
+    color: #ffffff;
+    transform: rotateY(180deg);
+    backface-visibility: hidden;
+  }
+
+  .front {
+    img {
+      border-radius: 2rem;
+    }
+  }
+}
+
+.container:hover > .card {
+  cursor: pointer;
+  transform: rotateY(180deg);
+}
+</style>
